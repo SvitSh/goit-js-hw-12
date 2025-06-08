3eb7ca0c0a97286f857c6daa02cdd12543e97dcc
Svitlana Shevchenko <920314ss@gmail.com>
2025-06-08 08:48:44 +0300

    hw12

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const myKey = '50629766-ca4e4f15305d377d5e3228fef';
+const url = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query, page = 1, perPage = 15) {
+  const params = {
+    key: myKey,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page,
+    per_page: perPage,
+  };
+
+  return axios.get(url, { params }).then(response => {
+    if (response.data.hits.length === 0) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    }
+    return response.data;
+  });
+}
